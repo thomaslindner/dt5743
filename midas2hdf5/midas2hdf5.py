28e287f8703f72a60c65a821c6b5e2527ed66e76
Thomas Lindner <lindner@triumf.ca>
2020-01-31 10:47:28 -0800

    Committing changes.

--- midas2hdf5/midas2hdf5.py
+++ midas2hdf5/midas2hdf5.py
@@ -4,12 +4,12 @@
 # Dec 2019
 #
 import sys
-sys.path.append("/home/lindner/packages/midas/python")
+sys.path.append("/Users/lindner/packages/midas/python")
 #import midas
 import midas.file_reader
 
 # Open our file
-mfile = midas.file_reader.MidasFile("/daq/daqstore/mpmttest/data/run00027sub000.mid")
+mfile = midas.file_reader.MidasFile("/Users/lindner/sample_data/mpmt/run00028sub000.mid.gz")
 
 event = mfile.read_next_event()
 
@@ -18,13 +18,28 @@ event = mfile.read_next_event()
 
 while event:
     bank_names = ", ".join(b.name for b in event.banks.values())
-    print("Event # %s of type ID %s contains banks %s" % (event.header.serial_number, event.header.event_id, bank_names))
+    print("Event # %s of type ID %s contains banks %s" % (event.header.serial_number,
+                                                          event.header.event_id, bank_names))
     
     for bank_name, bank in event.banks.items():
         if len(bank.data):
             print("    The first entry in bank %s is %x length: %i %s" % (bank_name, bank.data[0],len(bank.data),
                                                                           type(bank.data[0]).__name__))
-        if len(bank.data) > 10:
-            print("%x %x %x %x %x %x" % (bank.data[2],bank.data[3],bank.data[4],bank.data[5],bank.data[6],bank.data[7]))
-    
+
+
+            # Decode DT5743 bank (move this to separate python class eventually
+        if bank_name == "43FS":
+
+            grp_mask = (bank.data[3] & 0xff);
+            number_groups = 2  # do this calculation correctly            
+            num_sample_per_group = (len(bank.data) - 6)/ number_groups
+            
+
+            print("grp mask: %x sample_per_group %i " % (grp_mask,num_sample_per_group))
+            
+            print("%x %x %x %x %x %x" % (bank.data[2],bank.data[3],bank.data[4],bank.data[5],
+                                         bank.data[6],bank.data[7]))
+
+
+        
     event = mfile.read_next_event()
