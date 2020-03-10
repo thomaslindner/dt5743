375a469033cc3659b24d2952882fa283ddb77f75
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2020-03-10 12:46:30 -0700

    Update a_midas2hdf5.py

--- midas2hdf5/a_midas2hdf5.py
+++ midas2hdf5/a_midas2hdf5.py
@@ -18,13 +18,15 @@
 # update time using MIDAS and event time tag
 # store channels in different dataset/array format?
 
+# print statements commented out for now so code goes faster in cmd
+
 import sys
 import numpy as np
 import h5py
 
 sys.path.append("/home/mpmttest/online/dt5743/midas2hdf5")
-import a_TDT743_decoder#.a_TDT743_decoder
-from datetime import date
+import a_TDT743_decoder
+from datetime import date, datetime
 
 #sys.path.append("/Users/lindner/packages/midas/python")
 sys.path.append("/home/mpmttest/packages/midas/python")
@@ -38,7 +40,7 @@ mfile = midas.file_reader.MidasFile(filename)
 event = mfile.read_next_event()
 
 # Create an .hdf5 file, and open it for writting
-f_hdf5=h5py.File("".join([str(date.today()),"ScanEvents.hdf5"]),"w")
+f_hdf5=h5py.File("".join([str(datetime.now()),"ScanEvents.hdf5"]),"w")
 
 counter=0
 # We can simply iterate over all events in the file
@@ -47,12 +49,14 @@ while event:
     #grp=f_hdf5.create_group("".join(["Event #",str(event.header.serial_number)]))
     counter+=1
     grp=f_hdf5.create_group("".join(["Event #",str(counter)])) #temporary way
-#backet wasnt closed above, this may have been syntax issue below
-    #this was "invid syntax" bank_names = ", ".join(b.name for b in event.banks.values())
+
+    bank_names = ", ".join(b.name for b in event.banks.values())
     #print("Event # %s of type ID %s contains banks %s" % (event.header.serial_number,event.header.event_id, bank_names))
+
+    # add relevant metadata to group (attributes)
     grp.attrs["id"]=event.header.event_id
-    #grp.attrs["bank names"]=bank_names
-    #grp.attrs["number of banks"]=len(bank_names)
+    grp.attrs["bank names"]=bank_names
+    grp.attrs["number of banks"]=len(bank_names)
     grp.attrs["event time tag"]=0 #take from header
 
     hit_first=False
@@ -60,19 +64,14 @@ while event:
         # print first entry in the bank info
         if hit_first==False:
             hit_first=True
-            print("The first entry in bank %s is %x length: %i %s" % (bank_name, bank.data[0],len(bank.data),type(bank.data[0]).__name__))
+            #print("The first entry in bank %s is %x length: %i %s" % (bank_name, bank.data[0],len(bank.data),type(bank.data[0]).__name__))
 
         # a_TDT743_decoder decodes data and returns a np array, along with other useful info
-        # bank_array[1] = pmt analogue data, bank_array[0] = monitor pmt (CURRENTLY JUST PMT)
-        # ?what channels are currently plugged into digitizer?
-        #bank_name="test name"
+        # bank_array[1] = pmt analogue data, bank_array[0] = monitor pmt (CURRENTLY JUST PMT at ch0 and seperate arrays)
 
-        #initilize it first
         file_todecode=a_TDT743_decoder.a_TDT743_decoder(bank.data, bank_name)
         important_bank, ch0_arr, ch1_arr, number_groups, num_sample_per_group, group_mask=file_todecode.decoder()
 
-        # make decoder function work but for now just print here
-        # this just does on forgever print(bank_array)
 
         # Create a data set (numpy array) for all important banks
         # we will then fill this array using the decoder function variables
@@ -80,14 +79,15 @@ while event:
             dset=grp.create_dataset("ch0", ch0_arr.shape, data=ch0_arr)
             dset=grp.create_dataset("ch1", ch1_arr.shape, data=ch1_arr)
             #dset=grp.create_dataset(bank_name, bank_array.shape, data=bank_array)
-            # add relevant metadata (attributes)
+
+            # add relevant metadata to data set (attributes)
+            dset.attrs["name"]=bank_name
+            dset.attrs["number of groups"]=number_groups # will help with slicing
+            dset.attrs["samples per group"]=num_sample_per_group # will help with slicing
+            dset.attrs["group mask"]=group_mask
             #dset.attrs["temp"]=getTemp()
             #dest.attrs["time stamp"]=datetime.datetime.now() # change to midas
             #dset.attrs["laser settings"]=getLaser()
-            dset.attrs["name"]=bank_name
-            dset.attrs["number of groups"]=number_groups # will help with slicing the array
-            dset.attrs["samples per group"]=num_sample_per_group # will help with slicing the array
-            dset.attrs["group mask"]=group_mask
 
 
     event = mfile.read_next_event()
