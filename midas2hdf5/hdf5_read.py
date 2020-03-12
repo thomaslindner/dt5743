dcd9ea530a9e0950e5feee90251c74d16aea2e17
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2020-03-11 17:41:07 -0700

    Update hdf5_read.py

--- midas2hdf5/hdf5_read.py
+++ midas2hdf5/hdf5_read.py
@@ -6,7 +6,7 @@ import sys
 import numpy as np
 import h5py
 from datetime import date, datetime
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 class hdf5_read:
@@ -20,7 +20,7 @@ class hdf5_read:
         #self.individual_pmt=data_array[1]
         #loop to get vals before close, bring reading part up here
 
-    def min_vals_histo(self,bins):
+    def min_vals_histo(self,bins,title):
         '''
         makes historgram of pulses
         '''
@@ -43,23 +43,47 @@ class hdf5_read:
                     data_set=group[data_set_name].value
                     min_pulses.append(np.min(data_set))
 
-        print(min_pulses)# right now they are all .0 or just wrong
+        #print(min_pulses)#lots more noise and round numbers than usual?
 
 
         hdf5_file.close()
-        #plt.hist(min_pulses, bins)
-        #plt.yscale('log')
-        #plt.savefig('histogram1.png')
+        plt.hist(min_pulses, bins)
+        plt.yscale('log')
+        plt.xlabel('Waveform Minimum Value (ADC)')
+        plt.ylabel('Frequency')
+        plt.title(title)
+        plt.savefig(title)
 
-    def temp_vs_min():
+    def temp_vs_min(self):
         '''
         plots temperature versus pmt data
         '''
-        return None
+        hdf5_file=h5py.File(''.join([self.file_name, '.hdf5']), 'r')
+        min_pulses=[] #clarify exactly what is the x axis
+
+        #for group_name in self.hdf5_file:
+        #    for data_set in group_name:
+        #        min_pulses.append(min(data_set[0])) #are the numbers pure y vals?
+
+        keys=hdf5_file.keys()
+        groups=[]
+        for key in keys:
+            groups.append(hdf5_file[key])
+
+        dataset_keys=[]
+        for group in groups:
+            for data_set_name in group.keys():
+                if data_set_name=="ch0":
+                    data_set=group[data_set_name].value
+                    min_pulses.append(np.min(data_set))
+
+
 
 writename=sys.argv[1]
+plot_title=sys.argv[2]
+bins=sys.argv[3]
 
 test=hdf5_read("".join([writename,"ScanEvents"]))
 # program to have it take this from sys once this is working
 # test this with just basics working for now
-test.min_vals_histo(100)
+test.min_vals_histo(bins,plot_title)
