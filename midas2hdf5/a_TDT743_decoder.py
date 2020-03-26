c0f0928a7f78d43c97c2ccdc60afd39d4613d6c8
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2020-03-26 11:23:42 -0700

    Update a_TDT743_decoder.py

--- midas2hdf5/a_TDT743_decoder.py
+++ midas2hdf5/a_TDT743_decoder.py
@@ -1,118 +1,87 @@
-# hdf5 file reader for mpmt test stand digitizer data
-# Ashley Ferreira
-# March 2020
+# decoder for TDT743 digitizer
+# Thomas Lindner
+# Dec 2019
+#
+# Edits made by Ashley Ferreira, Feb 2020
 
-import sys
+# chaged so it only gives you indiv pmt data right now
+
 import numpy as np
-import h5py
-from datetime import date, datetime
-
-import matplotlib
-matplotlib.use('Agg')
-
-import matplotlib.pyplot as plt
 
 
-class hdf5_read:
+class a_TDT743_decoder:
     '''
-    after you call a_midas2hdf5.py to save MIDAS data 
-    reads hdf5 file created by a_midas2hdf5.py and interprets digitizer data
+    used in a_midas2hdf5.py to convert from MIDAS format to
+    key information needed to write to hdf5 file
     '''
-    def __init__(self, hdf5_file_name):
-        self.file_name=hdf5_file_name
-        #self.bank_array=#you need to open groups, you need to decide on structure
-        #self.monitor_pmt=data_array[0]
-        #self.individual_pmt=data_array[1]
-        #loop to get vals before close, bring reading part up here
+    def __init__(self, all_data, bank_name):
+        self.data=all_data
+        self.name=bank_name
 
-    def min_vals_histo(self):
+
+    def decoder(self):
         '''
-        makes historgram of pulses
+        decodes MIDAS data
         '''
-        hdf5_file=h5py.File(''.join([self.file_name, '.hdf5']), 'r') # you may specify file driver
-        min_pulses=[]
+        important_bank=False
+        decoded_arr=[]
 
-        #for group_name in self.hdf5_file:
-        #    for data_set in group_name:
-        #        min_pulses.append(min(data_set[0])) #are the numbers pure y vals?
+        #its constantly truggering thw wrong i dentifier below so this is commented out for now
+        #if self.data[0] != 0xa0000000: # memrory address? why does this have to be the first word? well its the identifier? just zero?
+        #    print("First word has wrong identifier; first word = 0x", self.data[0])
+        #    return important_bank, None, None, None, None, None
 
-        keys=hdf5_file.keys()
-        groups=[]
-        for key in keys:
-            groups.append(hdf5_file[key])
+        if self.name == "TEMP":
+            important_bank=True
+            # or no return as metadata? directly in the other one
+            # yeah for now this is handled in midas2hdf5
 
-        dataset_keys=[]
-        for group in groups:
-            for data_set_name in group.keys():
-                if data_set_name=="ch0":
-                    data_set=group[data_set_name].value
-                    min_pulses.append(np.min(data_set))
+        # Decode DT5743 bank
+        if self.name == "43FS":
+            important_bank=True
 
-        #print(min_pulses)#lots more noise and round numbers than usual?
+            # which one is the correct way of getting it? cpp just uses "GetChMask()"
+            # grp_mask = (self.data[3] & 0xff)
+            # group_mask = (self.data[3] & 0xff) + ((self.data[4] & 0xff000000) >> 16)
+            #
+            # just tried my way for now, confirm its .data[3]
+            group_mask=3#int(str(self.data[3]),2)
 
+            number_groups = 0
+            msk_str=str(self.data[3])
+            for i in range(len(msk_str)):
+                if msk_str[i]=="1":
+                    number_groups+=1
 
-        hdf5_file.close()
-        plt.hist(min_pulses, bin_num)#directly input bin num to get rid of the error
-        plt.yscale('log')
-        plt.xlabel('Waveform Minimum Value (ADC)')
-        plt.ylabel('Frequency')
-        plt.title(self.file_name)
-        plt.savefig(self.file_name)
+            num_sample_per_group = (len(self.data) - 6)/ number_groups # 4 instead of 6 in cpp code,  header -> 4 words
 
-    def temp_vs_min(self,title):
-        '''
-        plots temperature of 5 sensors versus pmt data
+            #print("grp mask: %x sample_per_group %i " % (group_mask,num_sample_per_group))
 
-        hdf5_file=h5py.File(''.join([self.file_name, '.hdf5']), 'r')
-        min_pulses=[]
-
-        temp1=[]
-        temp2=[]
-        temp3=[]
-        temp4=[]
-        temp5=[]
-
-        keys=hdf5_file.keys()
-        groups=[]
-        for key in keys:
-            groups.append(hdf5_file[key])
-
-        dataset_keys=[]
-        for group in groups:
-            for data_set_name in group.keys():
-                if data_set_name=="ch0":
-                    data_set=group[data_set_name].value
-                    min_pulses.append(np.min(data_set))
-                    #read in temperature vals
-                    all_temp=data_set["temp"]
-                    temp1.append(all_temp[0])
-                    temp2.append(all_temp[1])
-                    temp3.append(all_temp[2])
-                    temp4.append(all_temp[3])
-                    temp5.append(all_temp[4])
-
-        hdf5_file.close()
-        plt.plot(min_pulses, temp1, label="Sensor 1")
-        plt.plot(min_pulses, temp2, label="Sensor 2")
-        plt.plot(min_pulses, temp3, label="Sensor 3")
-        plt.plot(min_pulses, temp4, label="Sensor 4")
-        plt.plot(min_pulses, temp5, label="Sensor 5")
-        plt.legend(loc='best')
-        plt.xlabel('Temperature Sensor Reading (Deg)')
-        plt.ylabel('Waveform Minimum Value (ADC)')
-        plt.title('Min PMT Pulse Val vs. Temp for ',title)
-        plt.savefig(title)#this won't change but try it to see bugs
-        '''
-        return None
+            #print("%x %x %x %x %x %x" % (self.data[2],self.data[3],self.data[4],self.data[5],
+                                         self.data[6],self.data[7]))
 
 
+            #if group_mask == 3: # expand/generalize this later, once you start using more inputs
+                #print("group_mask = 0x3, this means group 0 and 1 are participating (channel 0 and 1 are taking input)")
 
-writename=sys.argv[1]
-#plot_title=sys.argv[2]
-#bins=sys.argv[3]
+            # try to get samples for first and second channel
+            samples_chan0 = []
+            samples_chan1 = []
+            for i in range(0, num_sample_per_group-1):
+                if i % 17 != 0:  # skip bogus data in every 17th sample.
+                    # don't completely understand what is happening below -ashley
+                    samples_chan0.append((self.data[6+i] & 0xfff))
+                    samples_chan1.append(((self.data[6+i] & 0xfff000) >> 12))
+            #print("Channel 0 samples:", samples_chan0)
+            #print("Channel 1 samples:", samples_chan1)
 
-test=hdf5_read("".join([writename,"ScanEvents"]))
-# program to have it take this from sys once this is working
-# test this with just basics working for now
-test.min_vals_histo()
-#test.temp_vs_min(plot_title)
+            #decoded_arr.append(samples_chan0)#samples_chan1])#these look like very promising numbers
+            #decoded_arr=np.array(decoded_arr).astype(np.float)
+
+            ch0_arr=np.array(samples_chan0)#.astype(np.float)
+            ch1_arr=np.array(samples_chan1)#.astype(np.float)
+
+            return important_bank, ch0_arr, ch1_arr, number_groups, num_sample_per_group, group_mask
+
+        # will return this if bank_name=/="43FS"
+        return important_bank, None, None, None, None, None
