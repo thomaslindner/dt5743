ef14f67ded3e5656a946c65522401782b74e8f96
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2020-04-30 07:25:13 -0700

    Delete test.py

--- midas2hdf5/test.py
+++
@@ -1,7 +0,0 @@
-
-import h5py
-import numpy as np
-#with h5py.File("mytestfile.hdf5", "w") as f:
-#    dset = f.create_dataset("mydataset", (100,), dtype='i')
-f = h5py.File("mytestfile.hdf5", "w")
-dset = f.create_dataset("mydataset", (100,), dtype='i')
