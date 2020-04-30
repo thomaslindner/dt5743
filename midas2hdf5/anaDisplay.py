ac7231a10bd6883dca5ee42ba9ba8346a8182fa3
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2020-04-30 12:11:08 -0700

    Update anaDisplay.py

--- midas2hdf5/anaDisplay.py
+++ midas2hdf5/anaDisplay.py
@@ -2,15 +2,13 @@ import sys
 import numpy as np
 import h5py
 
-import matplotlib
-#matplotlib.use('Agg')
-
 import matplotlib.pyplot as plt
 
 
-def waveform_display(run,event):
+def waveform_display(run,event,x_span):
     '''
-    displays waveform of specified event # and run #/filename
+    displays waveform of specified event and run number with the x axis
+    spanning 0 to x_span ns
     '''
     hdf5_file=h5py.File(''.join([run,'ScanEvents', '.hdf5']), 'r')
 
@@ -36,25 +34,23 @@ def waveform_display(run,event):
     ax.text(0.05, 0.5, textstr, transform=ax.transAxes, fontsize=10,
     verticalalignment='top', bbox=props)
 
-    #assuming its 0-300ns
-    x=np.linspace(0,300,length)
+    x=np.linspace(0,x_span,length)
     ax.plot(x, data_set, color='black')
     plt.ylabel('Waveform Minimum Value (ADC)')
-    plt.xlabel('Time (ns)') #you will need to specify the trigger value?
+    plt.xlabel('Time (ns)')
     plt.title(''.join([run,' event #', str(event_num),' DT743 Waveform for channel=0']))
     plt.show()
     #plt.savefig('Waveform Example')
 
 
 
-def online_waveform_display():
-    '''
-
-    '''
-    return None
-
 
 fname=sys.argv[1]
 event_num=int(sys.argv[2])
 
-waveform_display(fname,event_num)
+if len(sys.argv)==4:
+    time_span=int(sys.argv[3])
+else:
+    time_span=300 #sets default to 300ns by default
+
+waveform_display(fname,event_num, time_span)
