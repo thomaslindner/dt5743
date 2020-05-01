8c9dd28aa7c26cdd6654b04790c13d1f8ab810d4
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2020-05-01 08:31:21 -0700

    Delete plotting_helper.py

--- midas2hdf5/plotting_helper.py
+++
@@ -1,9 +0,0 @@
-import matplotlib
-import matplotlib.pyplot as plt
-
-def heat_plot(arr):
-    plt.imshow(arr)
-    plt.savefig('test_detectionEfficency')
-
-Z=np.random.random((10,10))
-heat_plot(Z)
